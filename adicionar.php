8e8d7c04c37c6ac82cfc541e0d79cc939a54bc3c
Tavilo Breno <breno_wk2@hotmail.com>
2022-01-05 20:10:06 -0300

    first commit

---
+++ adicionar.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Adicionar Usuário</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Adicionar Usuário</h1>
+	<br>
+	<form method="get" accept-charset="utf-8">
+		Nome: <br>
+		<input type="text" name="nome" required="" placeholder="Digite seu nome..."><br><br>
+		E-mail: <br>
+		<input type="text" name="email" required="" placeholder="Digite seu e-mail.."><br><br>
+		Telefone: <br>
+		<input type="text" name="telefone" required="" placeholder="Digite seu telefone"><br><br>
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Crud com PHP estrutural</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+
+	<h1>Lista de clientes</h1>
+	<br>
+	<a href="adicionar.php" title=""><button>Adicionar Usuário</button></a>
+	
+</body>
+</html>
