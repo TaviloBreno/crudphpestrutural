07d2242302036a4892fedfb1882c4e01ccd18e6a
Tavilo Breno <breno_wk2@hotmail.com>
2022-01-08 11:44:44 -0300

    Projeto completo

---
+++ READJME.md
@@ -0,0 +1,22 @@
+# Crud de clientes em PHP estrutural
+
+Crud de clientes com PHP estrutural usando composer para gerencimento de diretórios.
+
+## 🚀 Começando
+
+1. Criar o banco de dados para puxar as informações que serão utilizadas pelo sistema;
+2. Criar um arquivo de exposição dos clientes;
+3. Criar um arquivo de cadastro dos clientes;
+4. Criar um arquivo de atualização dos clientes; 
+5. Criar um arquivo de deleção dos clientes;
+6. Criar conexão entre os arquivos e o banco;
+7. Criar sessões para a ultilização de login do usuário e saber o tipo de cadastro do mesmo;
+
+### 🔧 Instalação
+
+1. Instalar o PHP na máquina;
+
+## 🛠️ Construído com
+
+1. PHP;
+2. Composer;
--- index.php
+++ index.php
@@ -1,16 +1,53 @@
+<?php
+
+if(isset($_POST['email']) && isset($_POST['senha'])) {
+
+    include('lib/conexao.php');
+
+    $email = $mysqli->escape_string($_POST['email']);
+    $senha = $_POST['senha'];
+
+    $sql_code = "SELECT * FROM clientes WHERE email = '$email'";
+    $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+
+    if($sql_query->num_rows == 0) {
+        echo "O e-mail informado é incorreto";
+    } else {
+        $usuario = $sql_query->fetch_assoc();
+        if(!password_verify($senha, $usuario['senha'])) {
+            echo "A senha informada está incorreta";
+        } else {
+            if(!isset($_SESSION))
+                session_start();
+            $_SESSION['usuario'] = $usuario['id'];
+            $_SESSION['admin'] = $usuario['admin'];
+            header("Location: clientes.php");
+        }
+    }
+
+}
+
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Crud com PHP estrutural</title>
-	<link rel="stylesheet" href="">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entrar</title>
 </head>
 <body>
-
-	<h1>Lista de clientes</h1>
-	<br>
-	<a href="adicionar.php" title=""><button>Adicionar Usuário</button></a>
-	
+    <h1>Entrar</h1>
+    <form action="" method="POST">
+        <p>
+            <label for="">E-mail</label>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label for="">Senha</label>
+            <input type="password" name="senha">
+        </p>
+        <button type="submit">Entrar</button>
+    </form>
 </body>
 </html>
